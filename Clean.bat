d7c4f91823652c017b0a974476390cd2800cc347
Hemoglobin <56655042+Hemoglobino@users.noreply.github.com>
2020-12-05 20:51:22 +0100

    Add files via upload

---
+++ Clean.bat
@@ -0,0 +1,25 @@
+@echo off
+
+echo ************
+echo it will delete all generated files from Robot or new pic in this path
+
+echo.
+echo *.png, log.html, report.html, output.xml
+echo ************
+
+
+
+pause
+
+del /s *.png
+del /s *.log
+del /s log.html
+del /s report.html
+del /s output.xml
+echo.
+echo.
+echo.
+
+
+pause
+
